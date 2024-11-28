1c021b7968755cdcb66baf3d7e9f523e0f4c7de1
Nikhil S Thomas <102505264+Nick12Thomas@users.noreply.github.com>
2024-11-28 17:08:03 +0530

    env config updated

--- lib/Models/networking.dart
+++ lib/Models/networking.dart
@@ -3,18 +3,16 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Networking {
-  // final String baseUrl = dotenv.env['BASE_URL'] ?? '';
-  // final String apiKey = dotenv.env['API_KEY'] ?? '';
-  // API_KEY=87f0283f-d431-416e-9087-73cd2f986db5
-  // BASE_URL=https://rest.coinapi.io/v1/exchangerate
+  final String baseUrl = dotenv.env['BASE_URL'] ?? '';
+  final String apiKey = dotenv.env['API_KEY'] ?? '';
 
   Future<double?> getExchangeRate(String baseCurrency, String targetCurrency) async {
-    final url = 'https://rest.coinapi.io/v1/exchangerate/$baseCurrency/$targetCurrency';
+    final url = '$baseUrl/$baseCurrency/$targetCurrency';
 
     try {
       final response = await http.get(
         Uri.parse(url),
-        headers: {'X-CoinAPI-Key': '87f0283f-d431-416e-9087-73cd2f986db5'},
+        headers: {'X-CoinAPI-Key': '$apiKey'},
       );
 
       if (response.statusCode == 200) {
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'price_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
